6b58dfae43cfb5b01895527f3fdba9725f041b5f
Felipe Djinn <felipe@felipedjinn.com.br>
2010-11-26 00:37:58 -0200

    add isGetAndAjax and isPostAndAjax tests

--- test/helpers/request_helper_test.php
+++ test/helpers/request_helper_test.php
@@ -2,6 +2,7 @@
 // Fake CodeIgniter basepath =)
 define('BASEPATH', true);
 
+require_once 'PHPUnit/Framework/Assert/Functions.php';
 require realpath('../../helpers/request_helper.php');
 
 class RequestHelperTest extends PHPUnit_Framework_TestCase
@@ -27,26 +28,55 @@ class RequestHelperTest extends PHPUnit_Framework_TestCase
     public function testIsGet()
     {
         $_SERVER['REQUEST_METHOD'] = 'GET';
-        $this->assertTrue(is_get());
+        assertTrue(is_get());
 
         $_SERVER['REQUEST_METHOD'] = 'POST';
-        $this->assertFalse(is_get());
+        assertFalse(is_get());
     }
 
     public function testIsPost()
     {
         $_SERVER['REQUEST_METHOD'] = 'POST';
-        $this->assertTrue(is_post());
+        assertTrue(is_post());
 
         $_SERVER['REQUEST_METHOD'] = 'GET';
-        $this->assertFalse(is_post());
+        assertFalse(is_post());
     }
 
     public function testIsAjax()
     {
-        $this->assertFalse(is_ajax());
+        assertFalse(is_ajax());
 
         $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
-        $this->assertTrue(is_ajax());
+        assertTrue(is_ajax());
     }
+
+    public function testIsGetAndAjax()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = null;
+
+        assertFalse(is_get());
+        assertFalse(is_ajax());
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+
+        assertTrue( is_get() && is_ajax() );
+    }
+
+    public function testIsPostAndAjax()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = null;
+
+        assertFalse(is_post());
+        assertFalse(is_ajax());
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+
+        assertTrue( is_post() && is_ajax() );
+    }
+
 }
